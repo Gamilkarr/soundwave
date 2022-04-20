dafc0c5b202bb755e882242147807605d5d0844f
Tata <gamilkarr@gmail.com>
2022-04-20 21:39:36 +0300

    add cli
    *add logger

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+import argparse
 import ffmpeg
 import random
+import logging
+
+
+def get_logger(name):
+    log_format = "%(asctime)s - [%(levelname)s] - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    shandler = logging.StreamHandler()
+    shandler.setLevel(logging.INFO)
+    shandler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(shandler)
+    return logger
 
 
 def movie_generate(
@@ -7,7 +21,6 @@ def movie_generate(
     bg_path,
     output_path,
     timestep,
-    _format,
 ):
     """Generates video from the soundtrack and background"""
     sound = ffmpeg.input(sound_path)
@@ -22,4 +35,34 @@ def movie_generate(
                 hue=random.randint(0, 360),
             )
     video = ffmpeg.overlay(bg, wave)
-    ffmpeg.output(sound, video, output_path, format=_format).run()
+    ffmpeg.output(sound, video, output_path, format="mp4").run()
+
+
+logger = get_logger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="generate video from the soundtrack and background"
+    )
+    parser.add_argument("soundtrack", help="set the path to the soundtrack")
+    parser.add_argument("background", help="set the path to the background")
+    parser.add_argument("output", help="set the path to the output file")
+    parser.add_argument(
+        "-t",
+        "--timestep",
+        default=0,
+        type=int,
+        help="set the color change time of the sound wave",
+    )
+    args = parser.parse_args()
+    try:
+        movie_generate(
+            args.soundtrack, args.background, args.output, args.timestep
+        )
+    except Exception as err:
+        logger.error(err)
+
+
+if __name__ == "__main__":
+    main()
